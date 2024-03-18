8f83e1621ad751f5ad9918accde95f6462f8aedf
anushishukla <144536251+anushishukla@users.noreply.github.com>
2024-03-18 19:46:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
+app.get(' /user/:userId', (req, res) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
+  const {userID} = req.params;
+  if(!userID)
+  {
+    return res.status(400).json({error:"userId might be missing or invalid"})
+  }
+  res.status(200).json({userID})
 });
 
 module.exports = app;
